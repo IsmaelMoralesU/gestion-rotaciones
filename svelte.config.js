9e5b5e60027e597c5b7c8ccee5645e3602ca771f
ismael <ismael1602m.u@gmail.com>
2025-09-25 23:29:22 -0600

    Configuración para GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,16 @@
-// Tauri doesn't have a Node.js server to do proper SSR
-// so we will use adapter-static to prerender the app (SSG)
-// See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
-  },
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/gestion-rotaciones' : ''
+    }
+  }
 };
 
 export default config;
