d852b088552c72f4b57899ab6badd4b887bd00fa
ismael <ismael1602m.u@gmail.com>
2025-09-28 15:56:32 -0600

    asi

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
       strict: true
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/nombre-de-tu-repositorio' : ''
+      base: process.env.NODE_ENV === 'production' ? '/gestion-rotaciones' : ''
     },
     prerender: {
       handleHttpError: 'warn',
